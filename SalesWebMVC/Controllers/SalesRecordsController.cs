42ad7dd210bba633c7c60b0fa7519092b20e0de1
Marcello Bronzatti <mbsimogaki@gmail.com>
2020-12-09 10:49:02 -0300

    implementing simple search

--- SalesWebMVC/Controllers/SalesRecordsController.cs
+++ SalesWebMVC/Controllers/SalesRecordsController.cs
@@ -7,20 +7,38 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Data;
 using SalesWebMVC.Models;
+using SalesWebMVC.Services;
 
 namespace SalesWebMVC.Controllers
 {
     public class SalesRecordsController : Controller
     {
+        private readonly SalesRecordService _salesRecordService;
+
+        public SalesRecordsController(SalesRecordService salesRecordService)
+        {
+            _salesRecordService = salesRecordService;
+        }
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult SimpleSearch()
+        public async Task<IActionResult> SimpleSearch(DateTime? initial, DateTime? final)
         {
-            return View();
+            if (!initial.HasValue)
+            {
+                initial = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!final.HasValue)
+            {
+                final = DateTime.Now;
+            }
+            ViewData["initial"] = initial.Value.ToString("yyyy-MM-dd");
+            ViewData["final"] = final.Value.ToString("yyyy-MM-dd");
+            var result = await _salesRecordService.FindByDateAsync(initial, final);
+            return View(result);
         }
 
         public IActionResult GroupingSearch()
---
+++ SalesWebMVC/Services/SalesRecordService.cs
@@ -0,0 +1,40 @@
+﻿using SalesWebMVC.Data;
+using SalesWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebMVC.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SalesWebMVCContext _context;
+
+        public SalesRecordService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? initial, DateTime? final)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (initial.HasValue)
+            {
+                result = result.Where(x => x.Date >= initial.Value);
+            }
+            if (final.HasValue)
+            {
+                result = result.Where(x => x.Date <= final);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+
+    }
+}
